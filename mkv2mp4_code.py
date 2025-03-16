b79b6aa82302d56e6650fa70fbda4f10b084a6df
Mina <98151101+MinaRavi@users.noreply.github.com>
2025-03-16 22:01:26 +0330

    the finding of mkv added

--- mkv2mp4_code.py
+++ mkv2mp4_code.py
@@ -8,3 +8,11 @@ def convert_to_mp4(mkv_file):
     out_name = name + '.mp4'
     ffmpeg.input(mkv_file).output(out_name).run()
     print('Finished Converting {}'.format(mkv_file))
+
+for path, folder, files in os.walk(start_dir):
+    for file in files:
+        if file.endwith('.mkv'):
+            print('Found 1 MKV file: %s' % file)
+            convert_to_mp4(os.path.join(start_dir,file))
+        else:
+            pass
