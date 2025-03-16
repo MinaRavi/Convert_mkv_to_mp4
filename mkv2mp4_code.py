4297028413d9489603daec2cb6827128303e6d89
Mina <98151101+MinaRavi@users.noreply.github.com>
2025-03-16 21:58:21 +0330

    the converter using ffmpeg

--- mkv2mp4_code.py
+++ mkv2mp4_code.py
@@ -1,2 +1,10 @@
 import os
 import ffmpeg
+start_dir = os.getcwd() #giving it the placed directory (looks for the same folder as this code)
+
+
+def convert_to_mp4(mkv_file):
+    name, ext = os.path.splitext(mkv_file)
+    out_name = name + '.mp4'
+    ffmpeg.input(mkv_file).output(out_name).run()
+    print('Finished Converting {}'.format(mkv_file))
